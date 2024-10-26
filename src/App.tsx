002b9fdd689f612577964775fd0271ad2ea076d5
Jeremiah Daniels <jedaniels3933@gmail.com>
2024-10-25 23:22:58 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import Counter from './components/Counter';
+import ShoppingCart from './components/ShoppingCart';
+import React from 'react';
+import './App.css';
+
+function App() {
+
+
+  return (
+    <>
+    <Counter /> 
+    <ShoppingCart /> 
+
+    </>
+  )
+}
+
+export default App
